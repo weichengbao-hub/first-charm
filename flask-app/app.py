327560ffe9492f0455f5a0d44fb28c8ac72bb4ff
Weicheng Bao <weicheng.bao@canonical.com>
2026-09-17 22:20:00 +0000

    update with database integration

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,13 +1,13 @@
-# initial hello world Flask app
-
 import flask
 
 app = flask.Flask(__name__)
+app.config.from_prefixed_env()
 
 
 @app.route("/")
 def index():
-    return "Hello, world!\n"
+    greeting = app.config.get("GREETING", "Hello, world!")
+    return f"{greeting}\n"
 
 
 if __name__ == "__main__":
